3e36ac8b44ed1f2eddba6f620f4a298178e04ce0
IhorC <igorchua@gmail.com>
2023-03-28 16:37:13 +0300

    task 1 completed

--- src/components/feedback/Feedback.jsx
+++ src/components/feedback/Feedback.jsx
@@ -1,31 +1,44 @@
+import PropTypes from "prop-types";
+import style from "./Feedback.module.css";
 import React, { Component } from "react";
+import { Statistics } from "./Statistics";
+import { FeedbackOptions } from "./FeedbackOptions";
+import { Section } from "./Section";
+
 
 export class Feedback extends Component {
-    state = {
-        good: 0,
-        neutral: 0,
-        bad: 0
+
+    onLeaveFeedback = (evt) => {
+        const elemName = evt.target.textContent.toLowerCase();
+        const state = this.props.state;
+        
+        this.setState({[elemName]: state[elemName] += 1});
+    }
+
+    countTotalFeedback = () => {
+        const {good, neutral, bad} = this.props.state;
+        return neutral + bad + good;
+    }
+
+    countPositiveFeedbackPercentage = () => {
+        const {good, neutral, bad} = this.props.state;
+        let feedbackSum = neutral + bad + good;
+
+        return (good > 0 && feedbackSum > 0) ? `${Math.floor(good / feedbackSum * 100)}%` : 0;
     }
 
     render() {
         return (
-            <section className="feedback">
-
-                <h1 className="feedback__title-main">Please leave feedback</h1>
-
-                <div className="feedback__buttons">
-                    <button className="feedback__btn">Good</button>
-                    <button className="feedback__btn">Neutral</button>
-                    <button className="feedback__btn">Bad</button>
-                </div>
-
-                <div className="feedback__stat">
-                    <h2 className="feedback__title">Statistics</h2>
-                    <p className="feedback__item">Good: </p>
-                    <p className="feedback__item">Neutral: </p>
-                    <p className="feedback__item">Bad: </p>
-                </div>
-            </section>
+            <Section title='Please leave feedback'>
+                <FeedbackOptions 
+                    onLeaveFeedback={this.onLeaveFeedback}
+                />    
+                <Statistics 
+                    state={this.props.state} 
+                    countTotalFeedback={this.countTotalFeedback} 
+                    countPositiveFeedbackPercentage={this.countPositiveFeedbackPercentage}
+                />
+            </Section>
         )
     }
 }
---
+++ src/components/feedback/Section.jsx
@@ -0,0 +1,14 @@
+import PropTypes from "prop-types";
+import style from "./Section.module.css";
+import { FeedbackOptions } from "./FeedbackOptions";
+import { Statistics } from "./Statistics";
+import { Children } from "react";
+
+export const Section = ({title, children}) => {
+    return (
+        <section className={style.feedback}>
+            <h1 className={style.titleMain}>{title}</h1>
+            {children}
+        </section>
+    )
+}
